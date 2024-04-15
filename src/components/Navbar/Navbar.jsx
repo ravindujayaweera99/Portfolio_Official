d41b5b497b5445d747982326feea905f3fa6ffcf
Ravindu Jayaweera <ravindujayaweera99@gmail.com>
2024-04-16 00:44:37 +0530

    Renovation Banner added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,27 +3,35 @@ import { Outlet, Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav>
-      <h1 className="logo">RJ</h1>
-      <ul>
-        <li>
-          <Link to={"/"}>Home</Link>
-        </li>
-        <li>
-          <Link to={"/skills"}>My Skills</Link>
-        </li>
-        <li>
-          <Link to={"/projects"}>My Projects</Link>
-        </li>
-        <li>
-          <Link to={"/aboutus"}>About Me</Link>
-        </li>
-        <li>
-          <Link to={"/contact"}><button className="hire-me">Hire Me</button></Link>
-        </li>
-      </ul>
-      <Outlet />
-    </nav>
+    <>
+    <div className="banner">
+      <h4>This Website is currently on Renovations!</h4>
+      <h3>New Look is on the Way!</h3>
+    </div>
+      <nav>
+        <h1 className="logo">RJ</h1>
+        <ul>
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"/skills"}>My Skills</Link>
+          </li>
+          <li>
+            <Link to={"/projects"}>My Projects</Link>
+          </li>
+          <li>
+            <Link to={"/aboutus"}>About Me</Link>
+          </li>
+          <li>
+            <Link to={"/contact"}>
+              <button className="hire-me">Hire Me</button>
+            </Link>
+          </li>
+        </ul>
+        <Outlet />
+      </nav>
+    </>
   );
 };
 
